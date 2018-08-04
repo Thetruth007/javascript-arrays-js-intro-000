9ec4780d134025c6dc7c95282abb80fd13cb90b9
rgossomjr@gmail.com <rgossomjr@gmail.com>
2018-08-04 02:14:42 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+chocolateBars.addElementToBeginningOfArray [1] = ...chocolateBars;{
+  return chocolateBars;
+}
+
+chocolateBars.destructivelyAddElementToBeginningOfArray [1] = ...chocolateBars;{
+  return chocolateBars;
+}
